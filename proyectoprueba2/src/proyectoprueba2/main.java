f43058a219c4e2756940dec3e22966b95cf2c851
benyiRivas <benyi.rivas@hotmail.com>
2016-10-21 11:51:29 -0500

    agregando un hola reyder

--- proyectoprueba2/src/proyectoprueba2/main.java
+++ proyectoprueba2/src/proyectoprueba2/main.java
@@ -5,7 +5,10 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo 2");
+		System.out.println("Los reyders");
 
 	}
+	
+	
 
 }
