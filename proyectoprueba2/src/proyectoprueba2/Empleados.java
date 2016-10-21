2f0a12eb77de75704cf078ab4f9e406dbf72b33f
Usuario <Usuario@DELL>
2016-10-21 12:08:12 -0500

    ADERIENDO LAS CLASES

---
+++ proyectoprueba2/src/proyectoprueba2/Empleados.java
@@ -0,0 +1,8 @@
+package proyectoprueba2;
+
+public class Empleados {
+	private int codigo;
+	private String Nombre, apellido;
+	
+
+}
---
+++ proyectoprueba2/src/proyectoprueba2/Sala.java
@@ -0,0 +1,5 @@
+package proyectoprueba2;
+
+public class Sala {
+
+}
