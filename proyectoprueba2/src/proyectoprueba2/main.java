150c0bdced689b5064e2c689536dc9260b501464
nielsenpuma <nielsenpumajihuallanca@mbp-de-nielsen>
2016-10-21 11:40:19 -0500

    Mundo 2

--- proyectoprueba2/src/proyectoprueba2/main.java
+++ proyectoprueba2/src/proyectoprueba2/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo 2");
 
 	}
 
